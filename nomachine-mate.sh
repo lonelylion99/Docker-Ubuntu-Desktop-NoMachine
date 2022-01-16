f0bafcad9c39533bf8eafd277a5bfd9509decf9c
lonelylion99 <89551706+lonelylion99@users.noreply.github.com>
2022-01-17 03:08:23 +0630

    Update nomachine-mate.sh

--- nomachine-mate.sh
+++ nomachine-mate.sh
@@ -19,6 +19,6 @@ read -p "Paste Ngrok Authtoken: " CRP
 ./ngrok authtoken $CRP 
 
 clear
-./ngrok tcp --region=ap 4000 &>/dev/null &
+./ngrok tcp --region ap 4000 &>/dev/null &
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
